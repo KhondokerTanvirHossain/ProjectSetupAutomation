34216ebda0a2c70a771ebd1269149a5b97f8bf48
Khondoker  Tanvir Hossain <tanvir@cellosco.pe>
2024-03-20 04:24:07 +0600

    instead of exception delete the existing folder and then copy paste

--- scripts/project_creator.py
+++ scripts/project_creator.py
@@ -2,9 +2,11 @@ import shutil
 import os
 
 def create_project_from_template(template_path, new_project_path):
+    template_name = os.path.basename(template_path)
+    new_project_path = os.path.join(new_project_path, template_name)
+
     if os.path.exists(new_project_path):
-        print(f"Project already exists at {new_project_path}")
-        return
+        shutil.rmtree(new_project_path)
 
     shutil.copytree(template_path, new_project_path)
     print(f"New project created at {new_project_path}")
